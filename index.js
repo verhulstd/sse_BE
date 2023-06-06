4e5a1a02402fd9e384bb37f6ca045076a102f67e
Verhulst David (p130024) <david.verhulst@ap.be>
2023-06-06 14:49:52 +0200

    rednershit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { nanoid } from "nanoid";
-import handleServerSentEvents from "./sse.js";
 
 const app = express();
 
@@ -44,7 +43,40 @@ app.get("/stuff", (req, res) => {
 });
 
 const clients = [];
-app.get("/sse", handleServerSentEvents(stuff, clients));
+app.get("/sse", (req, res) => {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
+
+  res.writeHead(200, headers);
+  const data = `data: ${JSON.stringify(stuff)}\n\n`;
+  res.write(data);
+
+  const client = {
+    time: Date.now(),
+    name: req.query.name,
+  };
+  clients.push({
+    ...client,
+    res: res,
+  });
+
+  setInterval(() => {
+    if (clients.length > 0) {
+      res.write(`data: \n\n`);
+    }
+  }, 5000);
+
+  req.on("close", () => {
+    console.log(`Connection closed from ${client.name}.`);
+    clients.splice(
+      clients.findIndex((c) => c.time === client.time),
+      1
+    );
+  });
+});
 
 function sendToAllClients() {
   clients.forEach((c) => c.res.write(`data: ${JSON.stringify(stuff)}\n\n`));
